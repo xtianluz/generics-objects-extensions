5e97c66f776795f8abd3b927586c9940b27ee2f3
KAZOMBIE <xtianluz@gmail.com>
2022-11-22 05:42:09 +0800

    generic class

--- app/src/main/java/com/example/genericsobjectsextensions/Generics.kt
+++ app/src/main/java/com/example/genericsobjectsextensions/Generics.kt
@@ -1,2 +1,36 @@
 package com.example.genericsobjectsextensions
 
+//This is a normal class
+//class FillInTheBlankQuestion(
+//    val questionText: String,
+//    val answer: String,
+//    val difficulty: String
+//)
+//
+//class TrueOrFalseQuestion(
+//    val questionText: String,
+//    val answer: Boolean,
+//    val difficulty: String
+//)
+//class NumericQuestion(
+//    val questionText: String,
+//    val answer: Int,
+//    val difficulty: String
+//)
+
+//below is refactored code using generic class
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+fun main(){
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+
+    println(question1.questionText + question2.answer)
+    println(question3.difficulty)
+}
